5ce663c20a8e38ae1dff72c8e8cc2a0fffe72128
Kritika S <kritikas.24@gmail.com>
2023-02-22 10:14:03 -0500

    Capacity to ship packages

--- PackageCapacity.java
+++ PackageCapacity.java
@@ -1,3 +1,15 @@
+/*A conveyor belt has packages that must be shipped from one port to another within days days.
+The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt (in the order given by weights). We may not load more weight than the maximum weight capacity of the ship.
+Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
+Input: weights = [3,2,2,4,1,4], days = 3
+Output: 6
+Explanation: A ship capacity of 6 is the minimum to ship all the packages in 3 days like this:
+1st day: 3, 2
+2nd day: 2, 4
+3rd day: 1, 4
+https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/description/
+*/
+
 class PackageCapaciy {
     public int shipWithinDays(int[] weights, int days) {
         int ans = 0;
